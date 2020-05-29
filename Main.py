e6c53dfd7fd01c38d76e4697cecab2ed31d7c288
Issa Nimaga <issa.nimaga@gmail.com>
2020-05-29 11:19:01 +0100

    Minor code cleanup and reformatting
    Added a couple of helpful comments and reformatted some section of the code.

--- Main.py
+++ Main.py
@@ -6,8 +6,9 @@ import PySimpleGUI as sg
 from os import path
 
 # Theme specification
-sg.theme('LightBlue6')	# Add a touch of color
+sg.theme('LightBlue6')
 
+# Menu Specification
 """
 # ------ Menu Definition ------ #      
 menu_def = [['File', ['Exit']],      
@@ -15,7 +16,8 @@ menu_def = [['File', ['Exit']],
 """
 
 hfs = 15 #hfs is short of horizontal field spacing
-# All the stuff inside your window.
+
+# All the stuff inside the window.
 layout = [  
             #[sg.Menu(menu_def, tearoff=True)], 
             #[sg.Text('Please enter details of the files below:', size=(45, 1), font=("Arial", 14), justification="center")],
@@ -24,7 +26,6 @@ layout = [
             [sg.Text('Document type', size=(hfs, 1)), sg.Radio('Report', "RADIO1", default=True, key='radioReport'), sg.Radio('Other', "RADIO1")],
             [sg.Text('Module 1', size=(hfs, 1)), sg.InputText(key='mod1')],
             [sg.Text('Module 2', size=(hfs, 1)), sg.InputText(key='mod2')],
-
             #Arianet theme section
             [sg.Text('Arianet Theme', size=(hfs, 1)) , sg.Frame(layout=[            
             [sg.Checkbox('Enterprise ICT', key='AT_cb1'), sg.Checkbox('Entreprise IT (incl Cloud)', key='AT_cb2')],
@@ -46,18 +47,17 @@ layout = [
             [sg.Text('Document ID', size=(hfs, 1)), sg.InputText( key='docIDinput', enable_events=True )],
             [sg.Text('Files', size=(hfs, 1)), sg.Text('undefined.pdf', key="fileName", size=(hfs*2, 1))],
             [sg.Text('Publisher', size=(hfs, 1)), sg.InputText('STL Partners',key='publisher')],
-            [sg.Text('Save location', size=(hfs, 1)), sg.FolderBrowse(target='-USER FOLDER-'), sg.Text('(Same as folder containing application by default)', key="pathShown"), sg.InputText(path.dirname(__file__),key='-USER FOLDER-', visible=False, enable_events=True)],
+            [sg.Text('Save location', size=(hfs, 1)), sg.FolderBrowse(target='-USER FOLDER-'), sg.Text('(Same as folder containing application by default)', key="pathShown", size=(hfs*2, 1)), sg.InputText(path.dirname(__file__),key='-USER FOLDER-', visible=False, enable_events=True)],
             [sg.Text(" ")],
             [sg.Button('Generate XML File'), sg.Button('Exit')] ]
 
-# Creation of the Window
+# Creation of the Main Window
 xml_icon = path.join(path.dirname(__file__),'xml.ico')
 window = sg.Window('Simple XML Generator - (Custom GUI app for STL Partners) - v0.1', layout, icon=xml_icon)
-#saveLocation = path.join(path.dirname(__file__), r'generated_file.xml') #default save location
 
 fileName  = "generated.xml" #Variable representing the name of file to save
 
-#Dictionary representing the properties of the XML
+# Instatiation of a Dictionary representing the properties of the XML file
 documentProperties = {
     "documentid":"",
     "title":"",
@@ -74,7 +74,8 @@ documentProperties = {
     "keywords":"",
     "files":""
 }
-#
+
+# A function that updates the document properties 
 def updateDocProperties():
     documentProperties["documentid"] = values['docIDinput']
     documentProperties["title"] = values['title']
@@ -87,7 +88,6 @@ def updateDocProperties():
     documentProperties["author"] = values['author']
     documentProperties["publisher"] = values['publisher']
     documentProperties["files"] = values['docIDinput']+".pdf"
-    #saveLocation = values['-USER FOLDER-']
 
     #Logic for the radio button
     if(values['radioReport']):
@@ -97,6 +97,7 @@ def updateDocProperties():
     #
 
     #Logic for the Arianet checkboxes
+    #It evaluates which boxes have been selected and saves the corresponding string selection
     selectedBoxes = []
     for i in range (1, 15):
         key2search = 'AT_cb' + str(i)
@@ -105,29 +106,27 @@ def updateDocProperties():
     documentProperties["themearianet"] = ",".join(str(w) for w in selectedBoxes)
     #
 
-def formatDictionaryForSaving(file):
-    file.write('<?xml version="1.0" encoding="UTF-8"?>')
-    file.write('\n<root>')
-    
-    for key in documentProperties:
-        file.write('\n    <' + key + '>')
-        file.write(documentProperties[key])
-        file.write('</' + key + '>')
+#Function for converting the dictionary with the file properties and saving it to an XML format
+def save_XML(saveLocation):
+    with open(saveLocation, 'w') as file:
 
-    file.write('\n</root>')
-    file.close()
+        file.write('<?xml version="1.0" encoding="UTF-8"?>')
+        file.write('\n<root>')
 
-#Function for saving xml file
-def save_XML(saveLocation, dict):
-    with open(saveLocation, 'w') as f:
-        formatDictionaryForSaving(f)
+        for key in documentProperties:
+            file.write('\n    <' + key + '>')
+            file.write(documentProperties[key])
+            file.write('</' + key + '>')
+
+        file.write('\n</root>')
+        file.close()
     sg.popup('XML generated!')
+
 
 def formatDateForXML(date):
     displayDate = date[:-9]
     pubdate= displayDate.replace("-","")
     documentProperties["pubdate"] = pubdate
-
     return displayDate
 
 # Event Loop to process "events" and get the "values" of the inputs
@@ -147,12 +146,13 @@ while True:
         updateDocProperties()
         fileName = documentProperties["documentid"] + '.xml'
         saveLocation = path.join(values['-USER FOLDER-'], fileName)
-        save_XML(saveLocation, documentProperties)
+        save_XML(saveLocation)
 
     elif event == 'docIDinput' and values['docIDinput'] and values['docIDinput'][-1] not in ('0123456789.'):
-        # if last character in input element is invalid, remove it
+        # If last character in input element is invalid, remove it
         window['docIDinput'].update(values['docIDinput'][:-1])
         #print(documentProperties)
+
     if event == 'docIDinput':
         window['fileName'].update(values['docIDinput']+".pdf")
 
